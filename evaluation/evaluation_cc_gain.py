c92e96ebd54f7e53fe8932eb5c68d6d63e4809c0
JohannaZitt <zittjohanna@gmail.com>
2023-12-05 12:24:58 +0100

    finished epoch loss evaluation

--- evaluation/evaluation_cc_gain.py
+++ evaluation/evaluation_cc_gain.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-with h5py.File('cc_gain.h5', 'r') as hf:
+with h5py.File('1_cc_gain.h5', 'r') as hf:
 
     #experiments = ['01_ablation_horizontal']
 
@@ -14,28 +14,37 @@ with h5py.File('cc_gain.h5', 'r') as hf:
         plt.figure(figsize=(14, 10))
 
         for data_type in experiment_group.keys():
-            print('\tData Type: ', data_type)
             data_type_group = experiment_group[data_type]
 
-            mean_cc_values = []
+            cc_values = []
 
             for seis_event in data_type_group.keys():
                 seis_event_group = data_type_group[seis_event]
                 data_cc = seis_event_group['data_cc'][:]
                 mean_cc = np.mean(data_cc)
-                mean_cc_values.append(mean_cc)
-                mean_cc_values = mean_cc_values[:40]
+                sd_cc = np.std(data_cc)
+                values = [mean_cc, sd_cc]
+                cc_values.append(values)
+
                 #print(data_cc.shape)
                 #data_cc_seis = seis_event_group['data_cc_seis'][:]
                 #print('\t\tData CC Seis: ', data_cc_seis)
 
-
+            cc_values = sorted(cc_values, key=lambda x: x[0])
+            cc_values = cc_values[:40]
+            cc_means = [item[0] for item in cc_values]
+            cc_stds = [item[1] for item in cc_values]
             ids = np.arange(1, 41)
 
+            print(np.shape(cc_stds))
+            print(np.shape(cc_means))
 
             # Plot erstellen
 
-            plt.scatter(ids, mean_cc_values, label = data_type)
+            plt.scatter(ids, cc_means, label = data_type)
+            plt.axhline(y = cc_means + cc_stds, color='green', linestyle='--')
+            plt.axhline(y = cc_means - cc_stds, color='green', linestyle='--')
+
         plt.xlabel('# Icequakes')
         plt.ylabel('CC gain [-]')
         plt.ylim(0, 5)
