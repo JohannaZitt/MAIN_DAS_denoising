c93bf37adc3d2f9283b4797bab4361364097fae1
JohannaZitt <zittjohanna@gmail.com>
2024-01-11 13:57:10 +0100

    improved plots

--- evaluation/plot_visual_assesment_epoch_loss.py
+++ evaluation/plot_visual_assesment_epoch_loss.py
@@ -13,35 +13,35 @@ import json
 
 '''
 
-models = ['ablation\nhorizontal', 'ablation\nvertical', 'accumulation\nvertical', ' accumulation\nhorizontal', 'stick-slip',
-          'surface', 'combined120', 'combined480', 'random480']
-data_types = ['stick-slip ablation', 'stick-slip accumulation', 'surface ablation', 'surface accumulation']
+models = ['Ablation\nHorizontal', 'Ablation\nVertical', 'Accumulation\nVertical', ' Accumulation\nHorizontal', 'Stick-slip',
+          'Surface', 'Combined120', 'Combined480', 'Random480']
+data_types = ['stick-slip ablation', 'stick-slip accumulation', 'surface accumulation', 'surface ablation']
 stick_slip_ablation = [4/49, 2/49, 6/49, 8/49, 6/49, 4/49, 4.5/49, 5.5/49, 2.5/49]
 stick_slip_accumulation = [4/41, 1.5/41, 8.5/41, 10.5/41, 8/41, 5/41, 5/41, 5/41, 4.5/41]
 surface_ablation = [2/44, 1.5/44, 4/44, 5.5/44, 3.5/44, 4/44, 2/44, 2/44, 2.5/44]
 surface_accumulation = [3/45, 1/45, 4/45, 3/45, 3.5/45, 1/45, 1.5/45, 2/45, 2/45]
 # bereits in rohen visible icequakes:
 raw_visible_quakes = [1/49, 18/41, 0, 36/45]
-visual_assesment_data = np.column_stack((stick_slip_ablation, stick_slip_accumulation, surface_ablation, surface_accumulation))
+visual_assesment_data = np.column_stack((stick_slip_ablation, stick_slip_accumulation, surface_accumulation, surface_ablation))
 visual_assesment_data_small = np.column_stack((stick_slip_ablation, stick_slip_accumulation))
 
 # plotting:
-bar_width = 0.2
+bar_width = 0.28
 fontsize = 12
 
 # Positionen der Balken
 bar_positions = np.arange(len(models))
 
 # Farben für die Balken
-blue = '#06a237'
-green = '#064ca2'
-light_blue = '#08e74f'
-light_green = '#4998f8'
+#blue = '#06a237'
+#green = '#064ca2'
+#light_blue = '#08e74f'
+#light_green = '#4998f8'
 
 colors = ['#66B2FF', '#0000FF', '#009900', '#66CC00']  # Beispiel: Rote, grüne, blaue und gelbe Balken
 
 
-fontsize = 12
+fontsize = 16
 
 ''' 1a  All - only category 2 
 
@@ -106,45 +106,47 @@ plt.show()
 
 
 ''' 1c All - category 1 and 2 vertauscht'''
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(16.2, 6))
 
 # Plot der Basislinie
-green = '#008786'
-light_green = '#7fc3c2'
+col_acc = '#008B93'#'#507EB9'#'#67A2E7' # '#9CB3FF'
+col_acc_light = '#9DE1E3'#'#A7D7FF' #'#AAE0FF' # '#DCE9FC'
 
-blue = '#870103'
-light_blue = '#c38081'
+col_abl = '#DF575F' #'#CC6B33'#'#7CD45F' # '#9CFFA9'
+col_abl_light = '#FFA09E' #'#FFB351'#'#A0F880' #'#D3F2D7'
 
-colors = [blue, green, blue, green]
-colors_light = [light_blue, light_green, light_blue, light_green]
-data_types_raw = ['raw stick-slip ablation', 'raw stick-slip accumulation', 'raw surface ablation', 'raw surface accumulation']
-
-# Plot der anderen Daten
+colors = [col_abl, col_acc, col_abl, col_acc]
+colors_light = [col_abl_light, col_acc_light, col_abl_light, col_acc_light]
+bar_positions = np.arange(1, 15, 1.7)
+print(bar_positions)
+alpha = 0.03
 for i in range(4):
     if data_types[i]=='stick-slip ablation' or data_types[i] == 'stick-slip accumulation':
-        plt.bar(bar_positions + i * bar_width, np.full(len(bar_positions), visual_assesment_data[:, i]), width=bar_width,
-                color=colors[i], hatch='//', zorder=3)
-        plt.bar(bar_positions + i * bar_width, visual_assesment_data[:, i] + raw_visible_quakes[i], width=bar_width,
-                color=colors_light[i], hatch='\\\\', zorder=2)
+        plt.bar(bar_positions + i * bar_width, np.full(len(bar_positions), visual_assesment_data[:, i]), width=bar_width - alpha,
+                color=colors[i], hatch='///',  linewidth = 0, edgecolor = colors_light[i], zorder=3) # edgecolor = 'grey',
+        plt.bar(bar_positions + i * bar_width, visual_assesment_data[:, i] + raw_visible_quakes[i], width=bar_width - alpha,
+                color=colors_light[i], hatch='///', linewidth = 0, edgecolor = colors[i], zorder=2)
     else:
-        plt.bar(bar_positions + i * bar_width, np.full(len(bar_positions), visual_assesment_data[:, i]), width=bar_width,
+        plt.bar(bar_positions + i * bar_width, np.full(len(bar_positions), visual_assesment_data[:, i]), width=bar_width- alpha,
                 color=colors[i], zorder=3)
-        plt.bar(bar_positions + i * bar_width, visual_assesment_data[:, i] + raw_visible_quakes[i], width=bar_width,
+        plt.bar(bar_positions + i * bar_width, visual_assesment_data[:, i] + raw_visible_quakes[i], width=bar_width- alpha,
                 color=colors_light[i], zorder=2)
 
 # Custom Legend:
 custom_lines = [mpatches.Patch(facecolor='none', hatch='//', edgecolor='black'),
                 mpatches.Patch(facecolor='none', edgecolor='black'),
-                mpatches.Patch(facecolor=light_green, edgecolor='black'),
-                mpatches.Patch(facecolor=green, edgecolor='black'),
-                mpatches.Patch(facecolor=light_blue, edgecolor='black'),
-                mpatches.Patch(facecolor=blue, edgecolor='black')]
-plt.legend(custom_lines, ['stick-slip', 'surface', 'accumulation-raw', 'accumulation', 'ablation-raw', 'ablation'], fontsize=fontsize, loc='upper left')
+                mpatches.Patch(facecolor=col_acc, edgecolor='black'),
+                mpatches.Patch(facecolor=col_acc_light, edgecolor='black'),
+                mpatches.Patch(facecolor=col_abl, edgecolor='black'),
+                mpatches.Patch(facecolor=col_abl_light, edgecolor='black')
+                ]
+plt.legend(custom_lines, ['Stick-slip', 'Surface', 'Accumulation', 'Accumulation raw', 'Ablation', 'Ablation raw'], fontsize=fontsize, loc='upper left', ncol=6, frameon = False, bbox_to_anchor=(0, 1.15))
 
 # Beschriftungen und Titel hinzufügen
-plt.ylabel('detection [%]', size=fontsize)
+plt.ylabel('Detection [%]', size=fontsize)
 plt.xticks(bar_positions + 0.3, models, rotation=15, fontsize=fontsize)
-plt.yticks([0.2, 0.4, 0.6, 0.8], [20, 40, 60, 80])
+#plt.yscale('log')
+plt.yticks([0.2, 0.4, 0.6, 0.8], [20, 40, 60, 80], fontsize=fontsize-4)
 plt.grid(axis='y', zorder=0)  # Gitterlinien für die y-Achse anzeigen
 plt.tight_layout()
 plt.savefig('plots/visual_assesnent_1_and_2_vertauscht.png')
@@ -272,22 +274,21 @@ bar_width = 0.2
 # Positionen der Balken
 bar_positions = np.arange(len(models))
 # Farben für die Balken
-#colors = ['#008786', '#870103']
-colors = ['#7fc3c2', '#c38081']
+colors = [col_acc, col_abl]
 # Labels für Legende
-data_types_small = ['accumulation zone', 'ablation zone']
+data_types_small = ['Accumulation zone', 'Ablation zone']
 
 # Plot des Balkendiagramms für jede Kategorie und jeden Wert
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(16.2, 6))
 for i in range(2):
-    plt.bar(bar_positions + i * bar_width, cc_data_small[:, i], width=bar_width, color=colors[i], label=data_types_small[i], zorder=i+2)
+    plt.bar(bar_positions + i * bar_width, cc_data_small[:, i], width=bar_width-alpha, color=colors[i], label=data_types_small[i], zorder=i+2)
 
 # Beschriftungen und Titel hinzufügen
-plt.ylabel('cc gain []', size=fontsize)
+plt.ylabel('CC gain []', size=fontsize)
 plt.axhline(y=1, color = 'black', zorder=5, )
 plt.grid(axis='y', zorder=0)
 plt.xticks(bar_positions + 0.1, models, rotation=15, size=fontsize) #  rotation=45, ha='left'
-plt.legend(fontsize=fontsize, loc='upper left')
+plt.legend(fontsize=fontsize, loc='upper left', ncol=6, frameon = False, bbox_to_anchor=(0, 1.15))
 
 
 plt.tight_layout()
