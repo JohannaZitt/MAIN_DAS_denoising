51312929a772f5f19b1c5a1f9bb9b972aa644181
JohannaZitt <zittjohanna@gmail.com>
2024-05-01 22:21:32 +0200

    added waterfall plots

--- plotting_waterfalls_plot.py
+++ plotting_waterfalls_plot.py
@@ -4,7 +4,6 @@ import os
 from scipy.signal import butter, lfilter
 from pydas_readers.readers import load_das_h5_CLASSIC as load_das_h5
 from datetime import datetime, timedelta
-from obspy import UTCDateTime
 
 def resample(data, ratio):
     try:
@@ -39,14 +38,14 @@ def load_das_data(folder_path, t_start, t_end, raw):
     # 2. downsample data in space:
     if raw:
         if data.shape[1] == 4864 or data.shape[1] == 4800 or data.shape[1] == 4928 :
-            data = data[:,::4]
+            data = data[:,::6]
         else:
-            data = data[:, ::2]
-        headers['dx'] = 8
+            data = data[:, ::3]
+        headers['dx'] = 12
 
     # 3. cut to size
-    ch_middel = int(3460/4) # get start and end channel:
-    data = data[:, ch_middel-40:ch_middel+40]
+    ch_middel = int(3842/6)
+    data = data[:, ch_middel - 100:ch_middel + 80]
 
     if raw:
         # 4. downsample in time
@@ -115,96 +114,95 @@ def plot_das_data(data, type="nix"):
         i += 1
     plt.show()
     #plt.savefig("plots/plot_" + type + ".png")
-'''
-Betrachten Eisbeben 90 und 106 von accumulation zone als wiggle plot mit cc gain geplottet denoist von 2 unterschiedlichen modellen
-'''
 
-accumulation_zone = True
-event_times = {90: "2020-07-06 19:10:51.0",
-               106: "2020-07-06 19:11:34.0"} #29
-experiments = ["07_combined120", "08_combined420"]
-experiment = experiments[0]
 
-raw_data_path = os.path.join("data", "raw_DAS", "0706/")
-denoised1_data_path = os.path.join("old/experiments", experiment, "denoisedDAS", "0706/")
+# Mögliche Events, die man plotten kann:
+#  - 2020-07-06 19:10:51.0
+#  - 2020-07-06 19:11:34.0
 
-id = 90
+event_times = {0: "2020-07-27 08:17:34.5", # Category 1, Receiver ALH
+               5: "2020-07-27 19:43:30.0", # Category 1, Receiver ALH
+               11: "2020-07-27 19:43:01.0", # Category 1, Receiver ALH
+               35: "2020-07-27 03:03:20.0", # Category 1, Receiver ALH
+               83: "2020-07-27 01:03:00.0", # Category 1, Receiver ALH
+
+               9: "2020-07-27 16:39:55.0", # Category 2, Receiver ALH
+               20: "2020-07-27 00:21:46.0", # Category 2, Receiver ALH
+               24: "2020-07-27 05:21:48.0", # Category 2, Receiver ALH
+               36: "2020-07-27 20:47:35.0", # Category 2, Receiver ALH
+               52: "2020-07-27 20:00:30.0", # Category 2, Receiver ALH
+               67: "2020-07-27 23:17:54.0", # Category 2, Receiver ALH
+               107: "2020-07-27 01:25:20.0", # Category 2, Receiver ALH
+               }
+experiment = "03_accumulation_horizontal"
+
+raw_path = os.path.join("data", "raw_DAS/")
+denoised_path = os.path.join("experiments", experiment, "denoisedDAS/")
+
+id = 107
 event_time = event_times[id]
+t_start = datetime.strptime(event_time, "%Y-%m-%d %H:%M:%S.%f")
+t_end = t_start + timedelta(seconds=3)
 
-t_start = datetime.strptime(event_time, '%Y-%m-%d %H:%M:%S.%f') #+ timedelta(seconds=6)
-t_end = t_start + timedelta(seconds=6)
+raw_data, raw_headers, raw_axis = load_das_data(raw_path, t_start, t_end, raw=True)
+denoised_data, denoised1_headers, denoised1_axis = load_das_data(denoised_path, t_start, t_end, raw=False)
 
-print(t_start)
-print(t_end)
+# Plot Data for Testing
+#plot_das_data(raw_data)
+#plot_das_data(denoised_data)
 
-raw_das_data, raw_headers, raw_axis = load_das_data(raw_data_path, t_start, t_end, raw=True)
-denoised1_das_data, denoised1_headers, denoised1_axis = load_das_data(denoised1_data_path, t_start, t_end, raw=False)
-
-plot_das_data(raw_das_data)
-plot_das_data(denoised1_das_data)
-
+# Calculate CC
 bin_size = 11
-raw_cc = compute_moving_coherence(raw_das_data, bin_size)
-denoised1_cc = compute_moving_coherence(denoised1_das_data, bin_size)
+raw_cc = compute_moving_coherence(raw_data, bin_size)
+denoised_cc = compute_moving_coherence(denoised_data, bin_size)
+raw_denoised_cc = denoised_cc / raw_cc
 
-raw_denoised1_cc = denoised1_cc / raw_cc
-
-print(raw_denoised1_cc.shape)
-
+# Parameters for Plotting:
 cmap = "viridis" # verschiednene colormaps: cmocean.cm.curl, seismic, cividis, plasma, inferno, viridis
 t_start = 0
-t_end = denoised1_das_data.shape[1]
+t_end = denoised_data.shape[1]
 ch_start = 0
-ch_end = denoised1_das_data.shape[0]
+ch_end = denoised_data.shape[0]
 ch_ch_spacing = 12
-vmin=-5
-vmax=5
+vmin=-7
+vmax=7
+fs = 16 # font size
+label = "main_icequake_cc_small_inferno"
 
-# 500 - 300 = 200 channel in total
-# 200 * 12m = 2400m
-
-fs = 14 # font size
-
-label = 'main_icequake_cc_small_inferno'
-
+# Basic Figure Settup:
 fig, ax = plt.subplots(1, 3,
                        gridspec_kw={
-                           'width_ratios': [2, 2, 1],
-                           'height_ratios': [1]},
+                           "width_ratios": [3, 3, 1],
+                           "height_ratios": [1]},
                       sharey = False)
 fig.set_figheight(8)
-fig.set_figwidth(20)
-ax[0].tick_params(axis='both', labelsize=fs)
-ax[1].tick_params(axis='both', labelsize=fs)
-ax[2].tick_params(axis='both', labelsize=fs)
-ax[0].set_title('Noisy DAS Data', y=1.05, fontsize = fs)
-ax[1].set_title('Denoised DAS Data', y=1.0, fontsize = fs)
-ax[2].set_title('CC',y = 1.0,  fontsize=fs)
+fig.set_figwidth(18)
+ax[0].tick_params(axis="both", labelsize=fs)
+ax[1].tick_params(axis="both", labelsize=fs)
+ax[2].tick_params(axis="both", labelsize=fs)
+ax[0].set_title("Raw DAS Data", y=1.0, fontsize=fs+4)
+ax[1].set_title("Denoised DAS Data", y=1.0, fontsize=fs+4)
+ax[2].set_title("CC", y = 1.0, fontsize=fs+4)
 ax[1].axes.yaxis.set_ticklabels([])
 ax[2].axes.yaxis.set_ticklabels([])
 ax[2].axes.xaxis.set_ticklabels([])
 #ax[2].set_xticks([0, 1, 2], [0, 1,2])
 
-
+# Plotting Raw Data
 plt.subplot(131)
-plt.imshow(raw_das_data[t_start:t_end, ch_start:ch_end].T, cmap=cmap, aspect="auto", interpolation="antialiased",
-          extent=(0 ,(t_end-t_start)/raw_headers["fs"],ch_start * ch_ch_spacing/1000,ch_end * ch_ch_spacing/1000),
+plt.imshow(raw_data, cmap=cmap, aspect="auto", interpolation="antialiased",
+          extent=(0 ,(t_end-t_start)/400,0,ch_end * ch_ch_spacing/1000),
           vmin=vmin, vmax=vmax)
-plt.xlabel("Time [s]", fontsize = fs)
-plt.ylabel("Offset [km]", fontsize = fs)
-plt.title('Noisy DAS Data', fontsize = fs)
-plt.xticks([0.2, 0.4, 0.6, 0.8], [0.2, 0.4, 0.6, 0.8])
-#plt.xticks([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
-#cbar = plt.colorbar(location = 'right')
-#cbar.set_label('strain rate [norm]', size=fs)
-#cbar.ax.tick_params(labelsize=fs)
+plt.xlabel("Time [s]", fontsize=fs)
+plt.ylabel("Offset [km]", fontsize=fs)
+plt.xticks([0.5, 1, 1.5, 2, 2.5], [0.5, 1, 1.5, 2, 2.5])
 
+# Plotting Denoised Data
 plt.subplot(132)
-plt.imshow(denoised1_das_data[t_start:t_end, ch_start:ch_end].T, cmap=cmap, aspect="auto", interpolation="antialiased",
-          extent=(0 ,(t_end-t_start)/denoised1_headers["fs"],ch_start * ch_ch_spacing/1000,ch_end * ch_ch_spacing/1000),
+plt.imshow(denoised_data, cmap=cmap, aspect="auto", interpolation="antialiased",
+          extent=(0 ,(t_end-t_start)/400,0,ch_end * ch_ch_spacing/1000),
           vmin=vmin, vmax=vmax)
-plt.xticks([0.2, 0.4, 0.6, 0.8], [0.2, 0.4, 0.6, 0.8])
-#plt.xticks([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+plt.xticks([0.5, 1, 1.5, 2, 2.5], [0.5, 1, 1.5, 2, 2.5])
 plt.xlabel("Time [s]", fontsize = fs)
 
 #cbar = plt.colorbar(location = 'right')
@@ -213,18 +211,20 @@ plt.xlabel("Time [s]", fontsize = fs)
 
 # Damit Graph gekippt angezeigt werden kann:
 x = np.arange(ch_end-ch_start)
-y_seis = raw_denoised1_cc[ch_start:ch_end]
+y_seis = raw_denoised_cc[ch_start:ch_end]
 X_seis = np.vstack((x, y_seis)).T
 X_seis = np.vstack((X_seis[:, 1], X_seis[:, 0])).T
 
+# Plotting CC Gain
 plt.subplot(133)
-plt.plot(X_seis[:, 0], X_seis[:, 1], color = 'black')
+plt.plot(X_seis[:, 0], X_seis[:, 1], color = "black")
 ax[2].invert_yaxis()
-plt.axvline(x=1, color='black', linestyle='dotted')
+plt.axvline(x=1, color="black", linestyle="dotted")
 plt.xlabel("CC gain []", fontsize = fs)
-plt.xticks([0, 1, 2, 3], [0, 1, 2, 3])
-# plt.legend()
+plt.xticks([0, 1, 2, 3, 4], [0, 1, 2, 3, 4])
+plt.ylim(0, raw_denoised_cc.shape[0]-1)
+
 
 plt.subplots_adjust(wspace=0.05)
 plt.show()
-#plt.savefig('plots/single_waveforms/'+str(id)+'_waterfall.png')
+#plt.savefig("plots/waterfall/"+str(id)+"_waterfall.png", dpi=400)
