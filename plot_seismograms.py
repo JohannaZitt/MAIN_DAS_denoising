dfdfbc2fa8416dab103a8d35563fb2df553f8bf0
JohannaZitt <zittjohanna@gmail.com>
2024-07-30 16:10:08 +0200

    finished frequency content

--- plot_seismograms.py
+++ plot_seismograms.py
@@ -78,7 +78,7 @@ def load_das_data(folder_path, t_start, t_end, raw, channel_delta_start, channel
 
     return data.T, headers, axis
 
-def plot_frequency_content_single_channel(raw_f_frq, raw_f_amp, denoised_f_frq, denoised_f_amp, font_size = 15, show_plot = True):
+def plot_frequency_content_single_channel(raw_f_frq, raw_f_amp, denoised_f_frq, denoised_f_amp, id, font_size = 15, show_plot = True):
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
@@ -87,23 +87,25 @@ def plot_frequency_content_single_channel(raw_f_frq, raw_f_amp, denoised_f_frq, 
     ax1.set_xlabel("Frequency [Hz]", fontsize=font_size)
     ax1.set_ylabel("Amplitude", fontsize=font_size)
     ax1.set_title("Noisy", fontsize=font_size + 4)
+    ax1.set_ylim(0, 120)
 
     # Second Subplot:
     ax2.plot(denoised_f_frq, denoised_f_amp, "k-")
     ax2.set_yticks([])
     ax2.set_xlabel("Frequency [Hz]", fontsize=font_size)
     ax2.set_title("Denoised", fontsize=font_size + 4)
+    ax2.set_ylim(0, 120)
 
     plt.tight_layout()
 
     if show_plot:
         plt.show()
     else:
-        #plot_name = ""
-        #plt.savefig("plots/frequency_content/" + plot_name + ".pdf", dpi=400)
+        plot_name = str(id) + "_fcontent_single_channel.pdf"
+        plt.savefig("plots/spectograms/fcontent_single_channel/" + plot_name, dpi=400)
         plt.close()
 
-def plot_frequency_content_multiple_channel(raw_data_freq, denoised_data_freq, raw_data, font_size = 15, show_plot = True, cc_spacing = 12, cmap = "plasma", aspect="auto", vmin=0, vmax=150):
+def plot_frequency_content_multiple_channel(raw_data_freq, denoised_data_freq, raw_data, id, font_size = 15, show_plot = True, cc_spacing = 12, cmap = "plasma", aspect="auto", vmin=0, vmax=150):
 
     extent = (0, raw_data.shape[0] * cc_spacing, 0, 250)
 
@@ -126,56 +128,66 @@ def plot_frequency_content_multiple_channel(raw_data_freq, denoised_data_freq, r
     if show_plot:
         plt.show()
     else:
+        plot_name = str(id) + "_fcontent_multiple_channel.pdf"
+        plt.savefig("plots/spectograms/fcontent_multiple_channel/" + plot_name, dpi=400)
         plt.close()
 
-def plot_seismogram_single_channel(raw_data, denoised_data, middle_channel, font_size=15, show_plot = True, NFFT=16, Fs=400, noverlap=8, cmap="plasma", scale_by_freq=True):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+def plot_seismogram_single_channel(raw_data, denoised_data, middle_channel, id, font_size=15, show_plot = True, NFFT=32, Fs=400, noverlap=16, cmap="plasma", scale_by_freq=True):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6), gridspec_kw={"width_ratios": [4, 5]})
 
     ax1.specgram(raw_data[middle_channel], NFFT=NFFT, Fs=Fs, noverlap=noverlap, cmap=cmap, scale_by_freq=scale_by_freq)
     ax1.set_title("Raw", fontsize=font_size + 4)
     ax1.set_ylabel("Frequency [Hz]", fontsize=font_size)
     ax1.set_xlabel("Time [s]", fontsize=font_size)
 
-    ax2.specgram(denoised_data[middle_channel], NFFT=NFFT, Fs=Fs, noverlap=noverlap, cmap=cmap, scale_by_freq=scale_by_freq)
+    spec2=ax2.specgram(denoised_data[middle_channel], NFFT=NFFT, Fs=Fs, noverlap=noverlap, cmap=cmap, scale_by_freq=scale_by_freq)
     ax2.set_title("Denoised", fontsize=font_size + 4)
     ax2.set_xlabel("Time [s]", fontsize=font_size)
     ax2.set_yticks([])
+    cbar = fig.colorbar(spec2[3], ax=ax2)
+    cbar.set_label("Power Spectral Density [s⁻²/Hz][dB]") #s=seconds
 
     plt.tight_layout()
 
     if show_plot:
         plt.show()
     else:
+        plot_name = str(id) + "_seismogram_single_channel.pdf"
+        plt.savefig("plots/spectograms/seismogram_single_channel/" + plot_name, dpi=400)
         plt.close()
 
 
 
 # ID : [starttime, channel middle, channel delta, category, closts seismometer]
-event_times = {0: ["2020-07-27 08:17:34.5", 40, 40, 1, "ALH"], # Category 1, Receiver ALH
+# Category 1: improved SNR
+# Category 2: new event detected
+# Category 3: worsening of an icequake through denoising
+# Category 4: no event
+event_times = {0: ["2020-07-27 08:17:34.6", 60, 70, 1, "ALH"], # Category 1, Receiver ALH
                5: ["2020-07-27 19:43:30.5", 45, 75, 1, "ALH"], # Category 1, Receiver ALH
-               11: ["2020-07-27 19:43:01.0", 1, "ALH"], # Category 1, Receiver ALH
-               35: ["2020-07-27 03:03:20.0", 1, "ALH"], # Category 1, Receiver ALH
-               83: ["2020-07-27 01:03:00.0", 1, "ALH"], # Category 1, Receiver ALH
+               11: ["2020-07-27 19:43:01.4", 65, 85, 1, "ALH"], # Category 1, Receiver ALH
+               35: ["2020-07-27 03:03:20.2", 65, 55, 1, "ALH"], # Category 1, Receiver ALH (dominant frequency at 100 Hz)
+               83: ["2020-07-27 01:03:00.2", 70, 25, 1, "ALH"], # Category 1, Receiver ALH
 
-               9: ["2020-07-27 16:39:55.0", 2, "ALH"], # Category 2, Receiver ALH
                20: ["2020-07-27 00:21:46.3", 30, 30, 2, "ALH"], # Category 2, Receiver ALH
-               24: ["2020-07-27 05:21:48.0", 2, "ALH"], # Category 2, Receiver ALH
-               36: ["2020-07-27 20:47:35.0", 2, "ALH"], # Category 2, Receiver ALH
-               52: ["2020-07-27 20:00:30.0", 2, "ALH"], # Category 2, Receiver ALH
-               67: ["2020-07-27 23:17:54.0", 2, "ALH"], # Category 2, Receiver ALH
-               107: ["2020-07-27 01:25:20.0", 2, "ALH"], # Category 2, Receiver ALH
+               24: ["2020-07-27 05:21:48.4", 70, 90, 2, "ALH"], # Category 2, Receiver ALH (dominant frequency at 100 Hz)
+               36: ["2020-07-27 20:47:34.8", 40, 45, 2, "ALH"], # Category 2, Receiver ALH
+               52: ["2020-07-27 20:00:30.4", 100, 85, 2, "ALH"], # Category 2, Receiver ALH
+               67: ["2020-07-27 23:17:54.0", 70, 35, 2, "ALH"], # Category 2, Receiver ALH
+               107: ["2020-07-27 01:25:19.6", 30, 15, 2, "ALH"], # Category 2, Receiver ALH
 
-               82: ["2020-07-27 05:04:55.0", 80, 150, 3, "ALH"], # Category 3, Receiver ALH
-               113: ["2020-07-27 18:22:59.0", 3, "ALH"] # Category 3, Receiver ALH
+               82: ["2020-07-27 05:04:55.8", 80, 150, 3, "ALH"], # Category 3, Receiver ALH (dominant frequency at 100 Hz)
+
                }
 experiment = "03_accumulation_horizontal"
 
 raw_path = os.path.join("data", "raw_DAS/")
 denoised_path = os.path.join("experiments", experiment, "denoisedDAS/")
 
-ids = [5, 20, 82]#[5, 20, 82]
+# real-world samples in paper: [5, 20, 82]
+ids = [0, 5, 11, 35, 83, 20, 24, 36, 52, 67, 107, 82]
 
-for i, id in enumerate(ids):
+for id in ids:
 
 
     event_time = event_times[id][0]
@@ -223,45 +235,44 @@ for i, id in enumerate(ids):
         denoised_data_freq[i] = np.abs(np.fft.rfft(denoised_data[i]))
     denoised_data_freq = denoised_data_freq[:, 0:250]
 
+    ###############################
+    # Plot waveform for reference #
+    ###############################
+    #channels=denoised_data.shape[0]
+    #for ch in range(channels):
+    #    plt.plot(denoised_data[-ch][:] + 1.5 * i, "-k", alpha=0.7)
+    #    i += 1
+    #plt.show()
+
+    #plt.plot(raw_data[middle_channel], color="green")
+    #plt.plot(denoised_data[middle_channel], color="pink")
+    #plt.show()
+
     ##############################
     # Plotting frequency content #
     ##############################
     plot_frequency_content_single_channel(raw_f_frq, raw_f_amp, denoised_f_frq, denoised_f_amp,
                                           font_size=15,
-                                          show_plot=False)
+                                          show_plot=False,
+                                          id=id)
     plot_frequency_content_multiple_channel(raw_data_freq, denoised_data_freq, raw_data,
                                             font_size=15,
                                             show_plot=False,
                                             cc_spacing=12,
                                             cmap="plasma",
                                             aspect="auto",
-                                            vmin=0, vmax=150)
-
-
+                                            vmin=0, vmax=150,
+                                            id=id)
     plot_seismogram_single_channel(raw_data, denoised_data, middle_channel,
                                    font_size=15,
-                                   show_plot=True,
+                                   show_plot=False,
                                    NFFT=32,
                                    Fs=400,
                                    noverlap=16,
                                    cmap="plasma",
-                                   scale_by_freq=True)
+                                   scale_by_freq=True,
+                                   id=id)
 
-
-
-
-
-
-
-
-    # Plotting spectogram of one channel.
-    #plt.specgram(raw_data[middle_channel], NFFT=256, Fs=400, noverlap=128, cmap="plasma")
-    #plt.colorbar()
-    #plt.show()
-
-    #plt.specgram(denoised_data[middle_channel], NFFT=256, Fs=400, noverlap=128, cmap="plasma")
-    #plt.colorbar()
-    #plt.show()
 
 
 
