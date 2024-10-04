f9b405f23999fd17a0c726ca6a048231033c37ee
JohannaZitt <zittjohanna@gmail.com>
2024-10-04 14:22:59 +0200

    add environment

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ Parts of the code are built upon the software provided by van den Endet et al. [
 
 
 
+data: https://zenodo.org/records/13868934?token=eyJhbGciOiJIUzUxMiJ9.eyJpZCI6IjNlZDUyMjdjLWEwN2UtNDkyNS05NWU0LTIxNTc1MGZhZWMyNSIsImRhdGEiOnt9LCJyYW5kb20iOiI0ZjIyNjgzZDIzODkyZDZhNmM2NWZhNWIwNDhkOWVmYyJ9.heSYoKnMCKEkR2dypJ7ga5_zqPlMdsIBJWaqhhsvL43zU0S1irRk4QeHw0EkM0hTXBcfdutLKwPgzgri-AKaqg
+article: 10.22541/au.172779667.76811452/v1
+
+
 [1] van den Ende, M., Lior, I., Ampuero, J.-P., Sladen, A., Ferrari, A. ve Richard, C. (2021, 3 Mart). A Self-Supervised Deep Learning Approach for Blind Denoising and Waveform Coherence Enhancement in Distributed Acoustic Sensing data. figshare. doi:10.6084/m9.figshare.14152277.v1
--- plotting_fig6.py
+++ plotting_fig6.py
@@ -1,10 +1,17 @@
 import csv
 import os
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 
-#experiments = os.listdir("experiments")
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+"""
+
+Here Figure 6 is generated. 
+
+"""
+
+
 experiments = ["01_ablation_horizontal", "02_ablation_vertical", "03_accumulation_horizontal", "04_accumulation_vertical",
                "05_combined200", "06_combined800", "07_retrained_combined200", "08_retrained_combined800",
                "09_borehole_seismometer"]
@@ -13,17 +20,16 @@ values_ablation_cc_gain_seis = {}
 values_accumulation_cc_gain = {}
 values_accumulation_cc_gain_seis = {}
 
-'''
 
-Extract Data:
 
-'''
 for experiment in experiments:
 
+    """ Path to cc values """
     csv_path = os.path.join("experiments", experiment, "cc_evaluation_" + experiment[0:2] + ".csv")
 
     with open(csv_path, "r") as file:
 
+        """ Read cc values """
         csv_reader = csv.DictReader(file)
 
         ablation_cc_gain = []
@@ -40,17 +46,16 @@ for experiment in experiments:
                 accumulation_cc_gain.append(float(row["mean_cc_gain"]))
                 accumulation_cc_gain_seis.append(float(row["mean_cross_gain"]))
 
+    """ Calculate Mean Values for Plot """
     values_ablation_cc_gain[experiment[3:]] = np.mean(ablation_cc_gain)
     values_ablation_cc_gain_seis[experiment[3:]] = np.mean(ablation_cc_gain_seis)
     values_accumulation_cc_gain[experiment[3:]] = np.mean(accumulation_cc_gain)
     values_accumulation_cc_gain_seis[experiment[3:]] = np.mean(accumulation_cc_gain_seis)
 
-'''
 
-Plot - cc_seis and local cc seperated
 
-'''
-
+""" Generate Local Waveform Coherence Subplot via datatype "local" """
+""" Generate Cross Correlation Subplot between DAS and seismometer data via datatype "seis" """
 datatypes = ["local", "seis"]
 for datatype in datatypes:
 
@@ -67,11 +72,11 @@ for datatype in datatypes:
         abl_cc_gain = list(values_ablation_cc_gain_seis.values())
         label = "Cross Correlation Seismometer Gain []"
 
-    # Params:
+    """ Parameters """
     col_acc = "#008B93"
     col_abl = "#DF575F"
-    width = 0.2  # Breite der Balken
-    x = np.arange(len(labels))  # x-Koordinaten für die Balken
+    width = 0.2  # depth of the bars
+    x = np.arange(len(labels))  # x-Koordinaten for bars
     fontsize = 12
     gain = 2 #fontsize gain
 
@@ -81,12 +86,10 @@ for datatype in datatypes:
     plt.bar(x + 0.5 * width, acc_cc_gain, width=width, label="Accumulation", color=col_acc, zorder=3)
 
     plt.axhline(y=1, color="blue", zorder=4)
-    # Achsenbeschriftungen und Titel hinzufügen
     plt.ylabel(label, fontsize=fontsize+gain)
-    #plt.yticks((1, 2, 3, 4), (1, 2, 3, 4), fontsize=fontsize)
-    #plt.legend(fontsize=fontsize+gain, frameon=False, ncol=1, loc="upper left")
     plt.xticks(x, labels, rotation=20, fontsize=fontsize+1)
 
+    """ Print Exact Values: """
     print(label)
     print("Labels", labels)
     print("Accumulation: ", acc_cc_gain)
@@ -94,71 +97,8 @@ for datatype in datatypes:
 
 
 
-    # Diagramm anzeigen
-
+    """ Save Plot """
     plt.tight_layout()
-    plt.show()
-    #plt.savefig("plots/cc/" + datatype + "_max.pdf", dpi=400)
-
-
-
-
-
-
-
-'''
-
-Pot Data - all in one
-
-
-
-
-# Data:
-labels = ["Ablation\nHorizontal", "Ablation\nVertical", "Accumulation\nHorizontal", "Accumulation\nVertical"
-          , "Combined 120", "Combined 480", "Random 480"]
-acc_cc_gain = list(values_accumulation_cc_gain.values())
-acc_cc_gain_seis = list(values_accumulation_cc_gain_seis.values())
-abl_cc_gain = list(values_ablation_cc_gain.values())
-abl_cc_gain_seis = list(values_ablation_cc_gain_seis.values())
-
-# Params:
-col_acc_local = "#008B93"
-col_acc_seis = "#9DE1E3"
-col_abl_local = "#DF575F"
-col_abl_seis = "#FFA09E"
-width = 0.14  # Breite der Balken
-x = np.arange(len(labels))  # x-Koordinaten für die Balken
-fontsize = 12
-
-plt.figure(figsize=(11, 6))
-plt.grid(axis='y', zorder = 0)
-plt.bar(x - 1.5 * width, acc_cc_gain, width=width, label='Acc Local', color = col_acc_local, zorder = 3)
-plt.bar(x - 0.5 * width, acc_cc_gain_seis, width=width, label='Acc Seis', color = col_acc_seis, zorder = 3)
-plt.bar(x + 0.5 * width, abl_cc_gain, width=width, label='Abl Local', color = col_abl_local, zorder = 3)
-plt.bar(x + 1.5 * width, abl_cc_gain_seis, width=width, label='Abl Seis', color = col_abl_seis, zorder = 3)
-plt.axhline(y=1, color = "blue", zorder=4)
-# Achsenbeschriftungen und Titel hinzufügen
-plt.ylabel('Gain []', fontsize=fontsize)
-plt.yticks(fontsize=fontsize)
-plt.xticks(x, labels, rotation=15, fontsize=fontsize)
-
-custom_lines = [mpatches.Patch(facecolor=col_acc_local, edgecolor='black'),
-                mpatches.Patch(facecolor=col_acc_seis, edgecolor='black'),
-                mpatches.Patch(facecolor=col_abl_local, edgecolor='black'),
-                mpatches.Patch(facecolor=col_abl_seis, edgecolor='black')
-                ]
-#plt.legend(custom_lines, ['Accumulation Local', 'Accumulation Seis', 'Ablation Local', 'Ablation Seis'], fontsize=fontsize,
-#           loc='upper left', ncol=4, frameon = True, bbox_to_anchor=(0, 0.1), facecolor = 'white')
-
-plt.legend(custom_lines, ['Accumulation Local', 'Accumulation Seis', 'Ablation Local', 'Ablation Seis'], fontsize=fontsize,
-           loc='upper left', ncol=4, frameon = False, bbox_to_anchor=(0, 1.1))
-
-# Diagramm anzeigen
-#plt.show()
-plt.savefig('plots/cc/all_in_one.png')
-'''
-
-
-
-
+    #plt.show()
+    plt.savefig("plots/fig_6_" + datatype + ".pdf", dpi=400)
 
